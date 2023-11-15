38beec784f3cdf728a6f2b81ea687d9af5f5a641
szabomeister <szabobeister@gmail.com>
2023-11-15 23:59:33 +0100

    added velocity visualization using first derivative; vector normal calculation

--- draw_bezier/draw_bezier.js
+++ draw_bezier/draw_bezier.js
@@ -1,11 +1,14 @@
 let slider;
 let addbutton;
 let points = Array();
+let line_segments = Array();
 let dragged_index=0;
 let spline_members = 3;
 let selected_index;
 let is_control_hidden = false;
 let is_control_line_hidden = false;
+let is_mirror_mode = false;
+let is_velocity_hidden = false;
 
 class Point {
   constructor(x,y) {
@@ -20,6 +23,30 @@ class Point {
   }
 }
 
+class ControlPoint extends Point {
+  constructor(x,y) {
+    super(x,y);
+  }
+}
+
+class LineSegment {
+  constructor() {
+    this.member_points = Array();
+  }
+  
+  toString() {
+    return "LineSegment";
+  }
+  
+  addPoint(p) {
+    this.member_points.push(p);
+  }
+  
+  getPoints() {
+    return this.member_points;
+  }
+}
+
 function calculatePoint(pts, t) {
   let p0 = pts[0];
   let p1 = pts[1];
@@ -31,11 +58,23 @@ function calculatePoint(pts, t) {
   return tmp;
 }
 
-function calculateHermite(pts, t) {
+function firstDerivative(pts, t) {
   let p0 = pts[0];
   let p1 = pts[1];
   let p2 = pts[2];
   let p3 = pts[3];
+  let px = p0.x * (-3 + 6*t - 3*t**2) + p1.x *( 3 - 12*t +9*t**2 ) + p2.x * (6*t - 9*t**2) + p3.x * 3*t**2;
+  let py = p0.y * (-3 + 6*t - 3*t**2) + p1.y *( 3 - 12*t +9*t**2 ) + p2.y * (6*t - 9*t**2) + p3.y * 3*t**2;
+  tmp = new Point(px, py);
+  return tmp;
+}
+
+
+function calculateHermite(pts, t) {
+  let p0 = pts[0];
+  let p1 = pts[1];
+  let p2 = pts[3];
+  let p3 = pts[2];
   let px = p0.x*(1 -3*t**2 +2*t**3)+p1.x*(t-2*t**2 +t**3)+p2.x*(3*t**2-2*t**3)+p3.x*(-1*t**2+t**3);
   let py = p0.y*(1-3*t**2+2*t**3)+p1.y*(t-2*t**2+t**3)+p2.y*(3*t**2-2*t**3)+p3.y*(-1*t**2+t**3);
   tmp = new Point(px, py);
@@ -52,7 +91,12 @@ function drawControllines(pts) {
 function mousePressed() {
   console.log(mouseX, mouseY);
   if (points.length < 4*spline_members) { //4
+    if (points.length % 4 == 1 || points.length % 4 == 2) {
+      points.push(new ControlPoint(mouseX, mouseY));
+    }
+    else {
     points.push(new Point(mouseX, mouseY));
+    }
   }
   else {
     for (let index in points) {
@@ -94,8 +138,16 @@ function mouseReleased() {
 function mouseClicked() {
   for (let p in points) {
     if(dist(points[p].x, points[p].y, mouseX, mouseY) < 12) {
-      console.log("selected: "+p);
+      //console.log("selected: "+p);
+      if (is_mirror_mode) {
+        let mirror_to = points[p];
+        points[int(selected_index)+1].x = points[selected_index].x + (points[selected_index].x - mirror_to.x); 
+        points[int(selected_index)+1].y = points[selected_index].y + (points[selected_index].y - mirror_to.y);
+        is_mirror_mode = false;
+      }
+      else {
       selected_index = p;
+      }
     }
   }
 }
@@ -118,6 +170,16 @@ function toggleHideControlLine() {
   is_control_line_hidden = !is_control_line_hidden;
 }
 
+function toggleVelocity() {
+  is_velocity_hidden = !is_velocity_hidden;
+}
+
+function enterMirrorMode() {
+  if (selected_index !== null && !(points[selected_index] instanceof ControlPoint)) {
+    is_mirror_mode = true;
+  }
+}
+
 function keyPressed() {
   if (key == 'd' ) {
     disjoint();
@@ -127,6 +189,15 @@ function keyPressed() {
   }
   else if (key == 'c') {
     toggleHideControlLine();
+  }
+  else if (key == 'm') {
+    enterMirrorMode();
+  }
+  else if (key == 'v') {
+    toggleVelocity();
+  }
+  else if (keyCode === ESCAPE) {
+    selected_index = null;
   }
 }
 
@@ -145,7 +216,7 @@ function addLineSegment() {
 
 function drawPoints(subpoints) {
    let prev = subpoints[0];
-   for (let t = 0; t< 1 ; t+=0.1) { //2
+   for (let t = 0; t<= 1.01 ; t+=0.01) { //2
      if (!is_control_line_hidden) {
        drawControllines(sub_points);
      }
@@ -165,7 +236,17 @@ function draw() {
   if (points.length !=0 && !is_control_hidden) {
     for (let p in points) {
       stroke(0,0,200);
+      
+      
+      if (points[p] == points[selected_index]) {
+        fill(200,200,200);
+      }
+      else if (points[p] instanceof ControlPoint) {
+        fill(100,100,250);
+      }
+      else {
       fill(0,255,0);
+      }
       circle(points[p].x, points[p].y, 25);
       //console.log("p:" +p);
     }
@@ -178,7 +259,7 @@ function draw() {
       points[dragged_index].x = mouseX;
       points[dragged_index].y = mouseY;
       if (points[dragged_index].joined_to !== null) {
-        console.log("joined: " + points[dragged_index].joined_to);
+        //console.log("joined: " + points[dragged_index].joined_to);
         points[dragged_index].joined_to.x = mouseX;
         points[dragged_index].joined_to.y = mouseY;
       }
@@ -192,21 +273,25 @@ function draw() {
     let bpt = calculatePoint(sub_points, remainder_part); // points, slider.value()
     fill(255);
     circle(bpt.x, bpt.y, 10);
+    // show velocity through first derivative
+    if (!is_velocity_hidden) {
+    let der = firstDerivative(sub_points, remainder_part);
+    let der_len = Math.sqrt(der.x**2 + der.y**2);
+    // make it unit length then scale by 100 to be visible
+    der.x = 100*(der.x / der_len);
+    der.y = 100*(der.y / der_len);
+    // calcuate normal vector
+    let normal = new Point(der.y, -der.x);
+    line(bpt.x, bpt.y, bpt.x +der.x, bpt.y +der.y);
+    line(bpt.x, bpt.y, bpt.x +normal.x, bpt.y +normal.y);
+    stroke(100,50, 10);
+    circle(bpt.x+ 0.2*normal.x, bpt.y+ 0.2*normal.y, 4);
+    circle(bpt.x- 0.2*normal.x, bpt.y- 0.2*normal.y, 4);
+    }
     
     let prev = points[0];
-    for (let t = 0; t< Math.floor(points.length / 4) ; t+=0.01) { //2
-      let integral_part = Math.floor(t);
-      ///let remainder_part = t - integral_part;
+    for (let integral_part = 0; integral_part <points.length/4 ; integral_part++) {
       sub_points = points.slice(4* integral_part, 4* (integral_part+1));
-      ///if (!is_control_line_hidden) {
-      ///  drawControllines(sub_points);
-      ///}
-      ///let pt = calculatePoint(sub_points, remainder_part); // points, t
-      
-      ///stroke(120,200,0);
-      ///line(prev.x, prev.y, pt.x, pt.y);
-      ///prev = pt;
-      //circle(pt.x, pt.y, 1);
       drawPoints(sub_points);
     }
   }
